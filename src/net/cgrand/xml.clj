d252c151fef2f9d1486487bcd2914179cc0d6cb8
Luke VanderHart <luke@vanderhart.net>
2015-06-07 14:27:07 -0400

    add back xml namespace for backwards compatibility

---
+++ src/net/cgrand/xml.clj
@@ -0,0 +1,22 @@
+(ns net.cgrand.xml
+  "Deprecated, provided for backwards compatibility
+
+  Most actual functionality has been moved into the
+  net.cgrand.parser NS."
+  (require [net.cgrand.parser :as p]
+           [net.cgrand.parser.sax :as sax]))
+
+(def tag p/tag)
+(def attrs p/attrs)
+(def content p/content)
+
+(def tag? p/tag?)
+
+(def comment? p/comment?)
+(def dtd? p/dtd?)
+
+(def xml-zip p/element-zipper)
+
+(def startparse-sax sax/startparse-sax)
+
+(def parse sax/parse)
